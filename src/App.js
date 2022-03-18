05757aa8b7f7ad3905e9165c6b02a004cf0023d9
Pedro Mendes <pedrohh0012@icloud.com>
2022-03-17 23:50:43 -0300

    Componentização do navbar e footer

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
+
 import Container from "./layout/Container";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
 
 function App() {
   return (
     <Router>
-      <ul>
-        <Link to="/">Home</Link>
-        <Link to="/company">Company</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/new-project">Project</Link>
-      </ul>
+      <Navbar />
       <Container customClass="min-height">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/company" element={<Company />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/new-project" element={<NewProject />} />
         </Routes>
       </Container>
-      <footer>footer</footer>
+      <Footer />
     </Router>
   );
 }
