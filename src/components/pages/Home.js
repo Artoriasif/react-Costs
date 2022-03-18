83738de8eee1be94b0c392c40ab0896d3df86861
Pedro Mendes <pedrohh0012@icloud.com>
2022-03-18 09:26:32 -0300

    Foi feito a página Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,17 @@
+import styles from "./Home.module.css";
+import savings from "../../image/savings.svg";
+import LinkButton from "../../layout/LinkButton";
+
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <section className={styles.homeContainer}>
+      <h1>
+        Bem-vindo ao <span>Costs</span>
+      </h1>
+      <p>Comece a gerecenciar os seus proejtos agora mesmo</p>
+      <LinkButton to="/new-project" text="Criar Projeto" />
+      <img src={savings} alt="Costs" />
+    </section>
+  );
 }
 export default Home;
