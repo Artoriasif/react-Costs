3e3c698ddcc89465cc79434a606dc29d74a63057
Pedro Mendes <pedrohh0012@icloud.com>
2022-03-18 09:43:30 -0300

    estrutura da pagina newproject e comp do form

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,4 +1,13 @@
+import ProjectForm from "../project/ProjectForm";
+import styles from "./NewProject.module.css";
+
 function NewProject() {
-  return <h1>New project</h1>;
+  return (
+    <div>
+      <h1>Criar projeto</h1>
+      <p>Crie seu proijetos para depois adicionar serviços</p>
+      <ProjectForm />
+    </div>
+  );
 }
 export default NewProject;
